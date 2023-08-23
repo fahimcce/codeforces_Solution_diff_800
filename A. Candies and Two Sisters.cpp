02904f15092a77a430c409cfc8c21ae70e552513
Fahim334712 <f.fahimcox@gmail.com>
2023-08-23 13:13:30 +0600

    candies and two sisters added

---
+++ A. Candies and Two Sisters.cpp
@@ -0,0 +1,26 @@
+// A. Candies and Two Sisters
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n == 1 || n == 2)
+        {
+            cout << 0 << endl;
+        }
+        else if (n % 2 != 0)
+        {
+            cout << n / 2 << endl;
+        }
+        else
+        {
+            cout << (n / 2) - 1 << endl;
+        }
+    }
+    return 0;
+}
