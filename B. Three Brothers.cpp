65060d78ec9e830d2c0ec6eb3ff6d8e60d83d9fa
Forhadul islam fahim <86653098+fahimcce@users.noreply.github.com>
2024-09-20 00:25:59 +0600

    Create B. Three Brothers.cpp

---
+++ B. Three Brothers.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;cin>>a>>b;
+    if((a+b) == 5) cout<<1<<endl;
+    else if((a+b) == 4) cout<<2<<endl;
+    else cout<<3<<endl;
+    return 0;
+    
+}
